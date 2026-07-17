69b12013f9e532935dd91719d617888474ae46f7
syaeful16 <syaefulloharnas16@gmail.com>
2026-07-17 10:55:56 +0700

    Move utils to registry and remove local file

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-// Fungsi cn (classNames) untuk menggabungkan class dengan aman
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
